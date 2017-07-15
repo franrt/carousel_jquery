4133bc03f7ab2c18bcde93ee57b852c05d18a345
Fran Ruiz-Tagle <francisca.ruiztagle@gmail.com>
2017-07-15 01:29:21 -0400

    jquery funciona pero con detalles

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,35 @@
 $(document).ready(function() {
 
-    var slider = $("#slider");
-    var siguiente = $("#btn-next");
-    var anterior = $("#btn-prev");
-
     //metodos que permiten mover elementos del dom
     $("#slider .slider__section:last").insertBefore("#slider .slider__section:first");
     //agregamos estilos para mostrar la primera img con margen -100%
-    slider.css("margin-left", "-" + 100 + "%");
+    $("#slider").css("margin-left", "-" + 100 + "%");
 
     function irDer() {
-        slider.animate({
+        $("#slider").animate({
             marginLeft: "-" + 200 + "%"
         }, 700, function() {
             //la imagen se moverá al final del slider
             $("slider .slider__section:first").insertAfter("#slider slider__section:last");
-            slider.css("margin-left", "-" + 100 + "%");
+            $(".slider").addClass(".slider");
         });
     }
 
-    siguiente.on("click", function() {
+    $("#btn-next").on("click", function() {
         irDer();
     });
 
     function irIzq() {
-        slider.animate({
+        $("#slider").animate({
             marginLeft: 0
         }, 700, function() {
             //la imagen se moverá al inicio del slider
             $("slider .slider__section:last").insertBefore("#slider .slider__section:first");
-            slider.css("margin-left", "-" + 100 + "%");
+            $(".slider__section").addClass(".slider");
         });
     }
 
-    anterior.on("click", function() {
+    $("#btn-prev").on("click", function() {
         irIzq();
     });
 
